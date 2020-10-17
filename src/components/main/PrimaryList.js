bc9259f7ab56d5870477532a5931c5f237874635
Kevin St.Onge <kevinstonge@gmail.com>
2020-10-17 18:24:40 -0400

    more work on components and layout

--- src/components/main/PrimaryList.js
+++ src/components/main/PrimaryList.js
@@ -1,13 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "../../styles/PrimaryList.scss";
 const PrimaryList = props => {
+    const [tabs, setTabs] = useState({ search: "active", favorites: "" });
+    const [query, setQuery] = useState("");
+    const [filter, setFilter] = useState("");
+    const search = e => {
+        e.preventDefault();
+        console.log(`searching for: ${query}`);
+    }
+    const filterFavorites = e => {
+        e.preventDefault();
+        console.log(`filtering: ${filter}`);
+    }
     return (
         <div className="primary-list-container">
             <div className="tabs">
-                <button>search</button><button>favorites</button>
+                <button className={tabs.search} onClick={()=>setTabs({search:"active",favorites:""})}>search</button>
+                <button className={tabs.favorites} onClick={()=>setTabs({search:"",favorites:"active"})}>favorites</button>
             </div>
-            <div className="list">
-                <p>list here</p>
+            <div className="list-container">
+                {tabs.search === "active" && (
+                    <form className="search-bar" onSubmit={search}>
+                        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)}>
+                        </input>
+                        <button type="submit">search</button>
+                    </form>
+                )}
+                {tabs.favorites === "active" && (
+                    <form className="filter-bar" onSubmit={filterFavorites}>
+                        <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)}>
+                        </input>
+                        <button type="submit">filter</button>
+                    </form>
+                )}
+                <div className="list">
+                    <p>list here</p>
+                </div>
             </div>
         </div>
     )

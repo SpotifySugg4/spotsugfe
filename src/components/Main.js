88365f3f5a49bdf53838ccfb36abba7b28b25d05
Kevin St.Onge <kevinstonge@gmail.com>
2020-10-19 22:08:53 -0400

    working on spotify api

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,23 +4,36 @@ import axios from "axios";
 import Details from "./main/Details";
 import PrimaryList from "./main/PrimaryList";
 import "../styles/Main.scss";
-import Spotify from 'spotify-web-api-node';
 const Main = props => {
+    const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+    const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+    const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
     const location = useLocation() || null;
-    const [code, setCode] = useState(new URLSearchParams(location.search).get('code'))
+    const [code] = useState(new URLSearchParams(location.search).get('code'))
     useEffect(() => {
         if (code == null) {
-            window.location.href=`https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}`
+            window.location.href=`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}`
         }
         else {
-            axios.post(`https://accounts.spotify.com/api/token?grant_type=authorization_code&code=${code}&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}`).then(r => console.log(r)).catch(e=>console.log(e));
+            const auth64 = btoa(`${clientId}:${clientSecret}`);
+            console.log("code: ", code);
+            console.log("auth64: ", auth64);
+            console.log(`https://accounts.spotify.com/api/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`);
+            axios.create(
+                {
+                    baseURL: "https://accounts.spotify.com/api",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Authorization": `Basic ${auth64}`,
+                    },
+                    body: {
+                        "grant_type": "authorization_code",
+                        "code": code,
+                        "redirect_uri": redirectUri,
+                    }
+                }).post(`/token`).then(r => console.log(r)).catch(e=>console.log(e));
         }
-    },[]);
-    const spotifyApi = new Spotify({
-        clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
-        redirectUri: process.env.REACT_APP_SPOTIFY_REDIRECT_URI
-    })
+    },[clientId,clientSecret,code,redirectUri]);
     
     return <div className="main">
         <PrimaryList />

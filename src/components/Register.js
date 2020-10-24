10553e57bd5486a79148a9a7fd9c631dd9223598
Kevin StOnge <kevinstonge@gmail.com>
2020-10-24 14:42:12 -0400

    fix

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,103 +1,83 @@
-import React, {useState, useEffect } from 'react';
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 // import axios from 'axios';
-import * as yup from 'yup'; 
+import * as yup from "yup";
 import { connect } from "react-redux";
 import { register } from "../actions/actions";
 
-const RegisterDiv = styled.div`
-  width: auto;
-  background: red;
-  color: black;
-  padding: 2%;
-  position: fixed;
-  margin: 2% 15% 15% 30%;
-  border-radius: 10px;
-  overflow: hidden;
-`;
 const Register = (props) => {
-
   const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    password: '',
+    name: "",
+    email: "",
+    password: "",
     // terms: false
-  })
+  });
 
-
-  const [buttonDisabled, setButtonDisabled] = useState(false)
-
-  const [users, setUsers] = useState([])
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   const [errors, setErrors] = useState({
-    name: '',
-    email: '',
-    password: '',
+    name: "",
+    email: "",
+    password: "",
     // terms: ''
-  })
+  });
 
-const formSchema = yup.object().shape({
-  name: yup
-    .string() 
-    .required('Please supply a username with a minumum of 4 characters.'), 
-  email: yup
-    .string()
-    .required('A valid email is required'),
-  password: yup
-    .string()
-    .required('Please supply a password with a minumum of 8 characters'),
-  terms: yup
-    .boolean()
-    .oneOf([true])
-})
+  const formSchema = yup.object().shape({
+    name: yup
+      .string()
+      .required("Please supply a username with a minumum of 4 characters."),
+    email: yup.string().required("A valid email is required"),
+    password: yup
+      .string()
+      .required("Please supply a password with a minumum of 8 characters"),
+    terms: yup.boolean().oneOf([true]),
+  });
 
   const completeForm = () => {
-    formSchema.isValid(formState)
-      .then(isValid => {
-        setButtonDisabled(!isValid)
-      })
-  }
-  useEffect(completeForm, [formState])
+    formSchema.isValid(formState).then((isValid) => {
+      setButtonDisabled(!isValid);
+    });
+  };
+  useEffect(completeForm, [formState]);
 
-
-  const validateChange = e => {
-
+  const validateChange = (e) => {
     yup
       .reach(formSchema, e.target.name)
       .validate(e.target.value)
-      .then(valid => {
+      .then((valid) => {
         setErrors({
           ...errors,
-          [e.target.name]: ''
-        })
+          [e.target.name]: "",
+        });
       })
-      .catch(error => {
+      .catch((error) => {
         setErrors({
           ...errors,
-          [e.target.name]: error.errors[0]
-        })
-      })
-  }
+          [e.target.name]: error.errors[0],
+        });
+      });
+  };
 
-
-  const changeHandler = e => {
-
-    e.persist()
+  const changeHandler = (e) => {
+    e.persist();
 
     const FormData = {
       ...formState,
-      [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
-    }
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value,
+    };
 
-    validateChange(e)
+    validateChange(e);
 
-    setFormState(FormData)
+    setFormState(FormData);
+  };
 
-  }
-
-  const formSubmit = e => {
-    e.preventDefault()
-    props.register({name: formState.name, email: formState.email, password: formState.password});
+  const formSubmit = (e) => {
+    e.preventDefault();
+    props.register({
+      name: formState.name,
+      email: formState.email,
+      password: formState.password,
+    });
     // axios
     //   .post('https://reqres.in/api/register', formState)
     //   .then(res => {
@@ -112,73 +92,56 @@ const formSchema = yup.object().shape({
     //   .catch(error => {
     //     console.log(error.response)
     //   })
-  }
+  };
 
   return (
-    
     <form onSubmit={formSubmit}>
-      <label htmlFor='name'>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-    <br></br>
-
+      <label htmlFor="name">
         <h1>Name:</h1>
-        <input 
-        id='name'
-        type='text'
-        name='name'
-        value={formState.name}
-        onChange={changeHandler}
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={formState.name}
+          onChange={changeHandler}
         />
 
-        {errors.name.length > 0 ? (<p className='error'>{errors.name}</p>): null}
-
+        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
       </label>
-    <br></br>
-    <br></br>
+      <br></br>
+      <br></br>
 
-      <label htmlFor='email'>
+      <label htmlFor="email">
         <h1>Email:</h1>
-        <input 
-        id='email'
-        type='text'
-        name='email'
-        value={formState.email}
-        onChange={changeHandler}
+        <input
+          id="email"
+          type="text"
+          name="email"
+          value={formState.email}
+          onChange={changeHandler}
         />
 
-        {errors.email.length > 0 ? (<p className='error'>{errors.email}</p>): null}
-
+        {errors.email.length > 0 ? (
+          <p className="error">{errors.email}</p>
+        ) : null}
       </label>
       <br></br>
       <br></br>
 
-
-      <label htmlFor='password'>
-      <h1>Create Password</h1>        
-      <input 
-        id='password'
-        type='password'
-        name='password'
-        value={formState.password}
-        onChange={changeHandler}
+      <label htmlFor="password">
+        <h1>Create Password</h1>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          value={formState.password}
+          onChange={changeHandler}
         />
 
-        {errors.password.length > 0 ? (<p className='error'>{errors.password}</p>): null}
-
+        {errors.password.length > 0 ? (
+          <p className="error">{errors.password}</p>
+        ) : null}
       </label>
-
 
       {/* <label htmlFor='terms' className='terms'>
         <input 
@@ -193,23 +156,21 @@ const formSchema = yup.object().shape({
         {errors.terms.length > 0 ? (<p className='error'>{errors.terms}</p>): null}
 
       </label> */}
-      <br>
-      </br>
-      <br>
-      </br>
-      <button disabled={buttonDisabled} type='submit'>Create My Account</button>
+      <br></br>
+      <br></br>
+      <button disabled={buttonDisabled} type="submit">
+        Create My Account
+      </button>
 
-      <pre>{JSON.stringify(users, null, 2)}</pre>
-        <p>{props.apiStatus}</p>
+      <p>{props.apiStatus}</p>
     </form>
-  )
+  );
+};
 
-}
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    apiStatus: state.apiStatus
-  }
-}
+    apiStatus: state.apiStatus,
+  };
+};
 
 export default connect(mapStateToProps, { register })(Register);
